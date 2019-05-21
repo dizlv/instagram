7bdc556ba89bcd1b51e23b80338ad1cc802d3f73
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2019-05-21 17:54:17 +0300

    New startup message.

--- instagram/__init__.py
+++ instagram/__init__.py
@@ -6,3 +6,5 @@ from instagram.application import create_application
 configuration = os.environ['APPLICATION_CONFIG_FILE']
 
 application = create_application(configuration=configuration)
+
+print('Starting application')
